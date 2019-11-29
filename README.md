b7a8080c5f5ed5bef53ccfa4d5f10efd4afb1ff5
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-11-29 15:51:21 -0800

    dev

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # semantic-db
 
-An event is data. Events have unique handles.
+An event is data. Events have unique handles. Events are queryable by handle.
+
+```rust
+use crate::{Event, Handle, query};
+
+pub fn main() {
+    let handle = Handle::new();
+    let event: Event = query(handle);
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+struct Event;
+struct Handle;
+struct Query;
+struct HandleGenerator;
+
+impl HandleGenerator {
+    pub fn create(&self) -> Handle {
+        Handle
+    }
+}
+
+fn query(handle: Handle) -> Event {
+    Event
+}
+fn custom_query(query: Query) -> Vec<Event> {
+    vec![]
+}
+
+fn make_query() -> Query {
+    Query
+}
+
+fn do_work() {
+    let generator = HandleGenerator;
+    let handle = generator.create();
+    let event: Event = query(handle);
+    let query: Query = make_query();
+    let events = custom_query(query);
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
